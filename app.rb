aa72e9e7daad62a1f8df8b7b2b2f8c691070dde6
Kevin Solorio <soloriok@gmail.com>
2016-12-24 00:10:54 -0800

    adding file writer on server side

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require 'sinatra'
 require 'pry'
+require 'json'
+require 'base64'
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  binding.pry
-  redirect '/'
+  data = JSON.parse(request.body.read)
+  File.open('./tmp/file.pdf', 'w') do |f|
+    f.write(Base64.decode64(data["pdf"]))
+  end
 end
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
-$(function(){
-  
+$(function() {
+  $('form').submit(function(event) {
+    var pdf, pdfInputNode, reader;
+
+    event.preventDefault();
+
+    pdfInputNode = $(this).find('input[type=file]')[0];
+    pdf = pdfInputNode.files[0];
+    reader = new FileReader();
+
+    reader.onload = function(event){
+      $.ajax({
+        url: '/',
+        method: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({"pdf": "data:application/pdf;base64 " + btoa(event.target.result)})
+      })
+    }
+    reader.readAsBinaryString(pdf)
+  })
 })
